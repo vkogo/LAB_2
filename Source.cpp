b57ee14e92962f75b2230fe03c6df9fb219d90a3
vkogo <zre92422@gmail.com>
2022-10-04 18:27:04 +0300

    new commit

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@ int main()
 	cout << "Enter value A ->  ";
 	cin >> a;
 
-	//z1 = cos(3 * PI / 8 - a / 4) * cos(3 * PI / 8 - a / 4) - (-cos(11 * PI / 8 + a / 4) * cos(3 * PI / 8 + a / 4));//correct
+	z1 = cos(3 * PI / 8 - a / 4) * cos(3 * PI / 8 - a / 4) - (-cos(11 * PI / 8 + a / 4) * cos(3 * PI / 8 + a / 4));//correct
 	z2 = sqrt(2) / 2 * sin(a / 2); //correct
 
 	cout << "RESULT Z1 = " << z1 << endl;
